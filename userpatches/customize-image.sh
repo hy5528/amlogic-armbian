6359b9396f4691b6dbd599b278f4fd4d3cb44907
hy5528 <95360525+hy5528@users.noreply.github.com>
2024-11-24 21:11:23 +0800

    Update customize-image.sh

--- userpatches/customize-image.sh
+++ userpatches/customize-image.sh
@@ -13,7 +13,11 @@
 # The sd card's root path is accessible via $SDCARD variable.
 
 # 安装 Docker
-wget -qO- https://get.docker.com/ |  sudo sh
+apt-get update
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list >/dev/null
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 # 安装 squeezelite
 sudo apt-get install squeezelite -y
 # 安装解压软件 unzip

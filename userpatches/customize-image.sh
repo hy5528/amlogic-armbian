264b092a2376f9ceac5684d4886783dc182b26d8
hy5528 <95360525+hy5528@users.noreply.github.com>
2024-11-24 21:55:40 +0800

    Update customize-image.sh

--- userpatches/customize-image.sh
+++ userpatches/customize-image.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 # 安装 Docker
-apt-get update
-curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list >/dev/null
-apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+curl -fsSL https://get.docker.com -o get-docker.sh
 # 安装 squeezelite
 sudo apt-get install squeezelite -y
 # 安装常用镜像
